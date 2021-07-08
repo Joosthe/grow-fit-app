9213fd57df72bca56385d1a7f4900e94523e545f
joosthe <joost@pau.be>
2021-07-08 15:51:58 +0200

    added router, home and 404 page

---
+++ src/Components/Pages/FourOhFour.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function FourOhFour() {
+  return (
+    <div className="fourohfour">
+      <h1>404</h1>
+      <p>u lost?</p>
+    </div>
+  )
+}
+
+export default FourOhFour;
