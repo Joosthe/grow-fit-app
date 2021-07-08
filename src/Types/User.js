e4588f4a90fa8adbc12aed6b146b5f94271ad4d9
joosthe <joost@pau.be>
2021-07-08 15:09:16 +0200

    added type user

---
+++ src/Types/User.js
@@ -0,0 +1,21 @@
+const user = {
+	id: 1, //number id 
+	first_name: '',
+	last_name: '',
+	username: '',
+	pasword: '',
+	nickname: '', // whil be an array of encouragement names
+	role: '', // wil choose out of an array of roles,
+	goals: [],// array of goals,
+	achievements_done: [], // array
+	workouts_done: [], 
+	workout_current: [], 
+	workout_planned: [],
+	sports: [] //this one is maybe for later 
+}
+
+const roles= [
+	'admin',
+	'coach',
+	'sporter',
+]
