a3e69629c20ad94abe20754c3765cf54258ce6a8
Your Name <your.email@gmail.com>
2021-09-16 09:15:24 +0200

    Added a working env file and loaded the variables in the app

--- src/Connections/firebase.js
+++ src/Connections/firebase.js
@@ -2,22 +2,14 @@ import firebase from "firebase";
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-  //apiKey: process.env.REACT_APP_FIREB_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREB_AUTHDOM,
-  // projectId: process.env.REACT_APP_FIREB_PROJID,
-  // storageBucket: process.env.REACT_APP_FIREB_STORBUCK,
-  // messagingSenderId: process.env.REACT_APP_FIREB_MESSSENDID,
-  // appId: process.env.REACT_APP_FIREB_APPID,
-  // measurementId: process.env.REACT_APP_FIREB_MEASUREID,
-
-  apiKey:"AIzaSyDSm80Eis2TwqSNl-mWF0JjN7J5FOEE4H8",
-  authDomain:"grow-fit-56859.firebaseapp.com",
-  projectId: "grow-fit-56859",
-  storageBucket: "grow-fit-56859.appspot.com",
-  messagingSenderId:"96870623929",
-  appId:"1:96870623929:web:2fc69bd6b33923a5994968",
-  measurementId:"G-HWXWNRV1TW"
-})
+  apiKey:            import.meta.env.VITE_FIREB_APIKEY,
+  authDomain:        import.meta.env.VITE_FIREB_AUTHDOMAIN,
+  projectId:         import.meta.env.VITE_FIREB_PROJECTID,
+  storageBucket:     import.meta.env.VITE_FIREB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREB_MESSAGINGSENDERID,
+  appId:             import.meta.env.VITE_FIREB_APPID,
+  measurementId:     import.meta.env.VITE_FIREB_MEASUREMENTID
+});
 
 export const auth = app.auth();
 export default app;
