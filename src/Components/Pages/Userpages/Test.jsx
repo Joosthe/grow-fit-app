729e8cb4bb454d28a13893924f0847ba73ea69a3
Your Name <your.email@gmail.com>
2021-09-30 11:34:35 +0200

    working upload test field added

--- src/Components/Pages/Userpages/Test.jsx
+++ src/Components/Pages/Userpages/Test.jsx
@@ -1,29 +1,106 @@
-import React from 'react'
+// import React from 'react'
+
+// export default function Test() {
+//   const handleTestUpload = (e) => {
+//     const fileUrl =  URL.createObjectURL(e.target.files[0]);
+//     uploadAsset(fileUrl);
+//   };
+
+//   function uploadAsset(fileUrl){
+//     const form = new FormData();
+//     form.append('fileUpload',fileUrl);
+//     for (var key of form.entries()) {
+//       console.log(key[0] + ', ' + key[1]);
+//   }
+  
+//   fetch(`${import.meta.env.VITE_GRAPHCMS_URL}/upload`, {
+//     method: 'POST',
+//     headers: {
+//       Authorization: `${import.meta.env.VITE_GRAPHCMS_AUTH}`,
+//     },
+//     body: form
+//   });
+//   }
+//   return (
+//     <div>
+//       <input type="file" onChange={handleTestUpload} />
+//     </div>
+//   )
+// } 
+
+
+
+// -----------
+
+
+import React, { useCallback, useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function Test() {
-  const handleTestUpload = (e) => {
-    const fileUrl =  URL.createObjectURL(e.target.files[0]);
-    uploadAsset(fileUrl);
-  };
+  const [files, setFiles] = useState([]);
 
-  function uploadAsset(fileUrl){
-    const form = new FormData();
-    form.append('fileUpload',fileUrl);
-    for (var key of form.entries()) {
-      console.log(key[0] + ', ' + key[1]);
-  }
-  
-  fetch(`${import.meta.env.VITE_GRAPHCMS_URL}/upload`, {
-    method: 'POST',
-    headers: {
-      Authorization: `${import.meta.env.VITE_GRAPHCMS_AUTH}`,
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file)
+          })
+        )
+      );
+
+      const form = new FormData();
+
+      form.append("fileUpload", acceptedFiles[0]);
+      // for (var key of form.entries()) {
+      //     console.log(key[0] + ', ' + key[1]);
+      // }
+      fetch(`${import.meta.env.VITE_GRAPHCMS_URL}/upload`, {
+        method: "POST",
+        headers: {
+          Authorization: `${import.meta.env.VITE_GRAPHCMS_AUTH}`
+        },
+        body: form
+      });
     },
-    body: form
+    [setFiles]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: "image/*",
+    onDrop
   });
-  }
+
+  useEffect(
+    () => () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
+
   return (
-    <div>
-      <input type="file" onChange={handleTestUpload} />
-    </div>
-  )
-} 
+    <>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drop or click to upload to GraphCMS</p>
+        )}
+      </div>
+      <aside>
+        {files.map((file, index) => (
+          <div key={file.name}>
+            <img
+              src={file.preview}
+              style={{ width: "100px", height: "100px" }}
+              alt=""
+            />
+          </div>
+        ))}
+      </aside>
+    </>
+  );
+}
+
+
