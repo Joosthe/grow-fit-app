5e818484f80fc57c43f5d3bfa0cb75b582c3ffbd
Your Name <your.email@gmail.com>
2021-10-12 13:15:44 +0200

    users can create an entry

--- src/Components/Pages/WorkoutPages/NewEntry.jsx
+++ src/Components/Pages/WorkoutPages/NewEntry.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import IntroSection from '../../PageSections/IntroSection';
 import Container from '../../Wrappers/Container';
 import './styles/NewEntry.scss'
@@ -9,7 +9,9 @@ import Select from 'react-select'
 import { getWorkoutsQuery } from '../../../Queries/Workout/getWorkoutsQuery';
 import useStaticCmsData from '../../../Hooks/useStaticCmsData';
 import BetterSelect from '../../PageComponents/FormElements/BetterSelect';
-//const today = new Date().toISOString(); 
+import { getData } from '../../../Connections/graphcsm';
+import { createEntryQuery, publishEntryQuery } from '../../../Queries/entry/createEntryQuery';
+import { useUser } from '../../../Contexts/UserContext';
 
 // const options = Workouts.map((item)=> {
 //   return{'value': item.id, 'label': item.title}
@@ -19,13 +21,37 @@ export default function NewEntry() {
   const sc = useStaticContent('WorkoutPages.NewEntry');
   const [selectedWorkout, setSelectedWorkout] = useState('');
   const { data: workouts } = useStaticCmsData(getWorkoutsQuery);
-
+  const {currentUser} = useUser();
+  const entryInfo = useRef(); 
+  const entryScore = useRef();
+  function selectWorkout(workout){
+    setSelectedWorkout(workout);
+  }
 
   function handleSubmit(e){ 
     e.preventDefault();
-    console.log(selectedWorkout);
-  }
 
+    try{
+      alert('heelo wolrld')
+      getData(createEntryQuery(
+        currentUser.id,
+        selectedWorkout.id, 
+        entryScore.current.value,
+        entryInfo.current.value
+        )).then(
+          data =>{
+            console.log(data);
+            getData(publishEntryQuery(data.createEntry.id));
+            entryScore.current.value= "";
+            entryInfo.current.value="";
+            setSelectedWorkout('');
+            
+          }
+        );
+    }catch(err){
+      console.error(err);
+    }
+  }
 
   return (
     <>
@@ -42,20 +68,20 @@ export default function NewEntry() {
           <label htmlFor="entry-workout">Select your work-out</label>
           <BetterSelect
                 selectionData={workouts.workouts} 
-                onSelect={(e)=> {setSelectedWorkout(e.value)}}
+                onSelect={selectWorkout}
                 placeHolder="Type to filter"
               />
         </div>
         <div className="form-element custom-class">
           <label htmlFor="entry-extra-info">How was it ?</label>
-          <p className=" "><input type="number" min={0} max={10} step={1}/>/ 10</p>
+          <p className=" "><input type="number" min={0} max={10} step={1} ref={entryScore}/>/ 10</p>
         </div>
        
       </div>
       <div className="form-right">
         <div className="form-element">
           <label htmlFor="entry-extra-info">Extra info</label>
-          <textarea name="entry-extra-info" id=""></textarea>
+          <textarea name="entry-extra-info" id="" ref={entryInfo}></textarea>
         </div>
 
       </div>
