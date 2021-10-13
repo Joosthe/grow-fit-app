e29cef3315906030c20eb12424dcd3f655236903
Your Name <your.email@gmail.com>
2021-10-13 10:57:16 +0200

    make select required

--- src/Components/PageComponents/FormElements/Select.jsx
+++ src/Components/PageComponents/FormElements/Select.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Select from 'react-select'
 import styles from './styles/Select.module.scss';
-export default function CustomSelect(props) {
 
+
+export default function CustomSelect(props) {
   return (
     <div className={styles.simple__select}>
-      <Select classNamePrefix='filter' options={props.options} onChange={(e)=>(props.onChange(e))}  />
+      <Select required classNamePrefix='filter' options={props.options} onChange={(e)=>(props.onChange(e))}  />
     </div>
   )
 }
