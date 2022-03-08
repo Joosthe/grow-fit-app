9d6a9666018d1271aca7a8539fdae083fe5c42b8
joost hermans <joost@pau.be>
2022-03-08 17:43:57 +0100

    update path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
+// import { defineConfig } from 'vite'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
+// import { resolve } from 'path'
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+//   base: './',
+//   publicDir: 'public',
+//   resolve: { alias: { 
+//     '@': '/src',
+//     '@detail': '/src/Components/Pages/WorkoutPages'
+//   }},
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         nested: resolve(__dirname, './nested/index.html')
+//       }
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: './',
-  publicDir: 'public',
-  resolve: { alias: { 
-    '@': '/src',
-    '@detail': '/src/Components/Pages/WorkoutPages'
-  }},
+   resolve: { alias: { 
+     '@': '/src',
+     '@detail': '/src/Components/Pages/WorkoutPages'
+   }},
 })
