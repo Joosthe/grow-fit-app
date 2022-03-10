2da3a63184cfbba81ee040a7b28efa209acfd6fc
joost hermans <joost@pau.be>
2022-03-10 07:59:45 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -24,15 +24,25 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // css: { postcss: { plugins: [ {
+  //   postcssPlugin: 'internal:charset-removal',
+  //   AtRule: {
+  //     charset: (atRule) => {
+  //       if (atRule.name === 'charset') {
+  //         atRule.remove();
+  //       }
+  //     }
+  //   }
+  // }]}},
   plugins: [reactRefresh()],
-  resolve: { alias: { 
-    '@':              '/src',
-    '@generalPage':   '/src/Pages/GeneralPages',
-    '@userPage':      '/src/Pages/UserPages',
-    '@workoutPage':   '/src/Pages/WorkoutPages',
-    '@PageComponent': '/src/Components/PageComponents/', 
-    '@PageSection':   '/src/Components/PageSections',
-    '@Wrapper':       '/src/Components/Wrappers',
-    '@Navigation':    '/src/Components/Navigation'
-  }},
+    resolve: { alias: { 
+      '@':              '/src',
+      '@generalPage':   '/src/Pages/GeneralPages',
+      '@userPage':      '/src/Pages/UserPages',
+      '@workoutPage':   '/src/Pages/WorkoutPages',
+      '@PageComponent': '/src/Components/PageComponents/', 
+      '@PageSection':   '/src/Components/PageSections',
+      '@Wrapper':       '/src/Components/Wrappers',
+      '@Navigation':    '/src/Components/Navigation'
+   }},
 })
